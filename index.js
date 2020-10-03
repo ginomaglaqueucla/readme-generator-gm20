89db006c996493f082ba1c9246b095e5d7fb4272
ginomaglaque <ginomaglaque@gmail.com>
2020-10-02 19:36:30 -0700

    promptUser function

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+const inquirer = require('inquirer');
 // array of questions for user
 const questions = [
-
+    'Enter GitHub username:',
+    'Enter email contact:',
+    'What is the project title:',
+    'Enter project description:',
+    'Enter installation instructions:',
+    'Enter usuage information:',
+    'Enter contribution guidelines:',
+    'Enter Tests:'
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
 }
 
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'gitUser',
+        message: questions[0],
+        validate: gitUser => {
+          if (gitUser) {
+            return true;
+          } else {
+            console.log('REQUIRED! Enter GitHub username!');
+            return false;
+          }
+        }
+      }
+    ]);
+}
+
 // function to initialize program
 function init() {
-
+    promptUser();
 }
 
 // function call to initialize program
